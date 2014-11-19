c19d3e657bdb63658ce629bd96f8918ba1e78fc0
simplay <michael.single@students.unibe.ch>
2014-11-19 16:41:24 +0100

    Initial Commit: Javaassist plus JRuby

---
+++ sde_project/src/main/java/sde_project/sde_project/App.java
@@ -0,0 +1,13 @@
+package sde_project.sde_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
