c9ee81992f0c3ae4dee5a2f1ebf9de8e5a81747e
simplay <michael.single@students.unibe.ch>
2014-11-19 16:56:55 +0100

    Added a placeholder call to show how to load/write a pool

--- sde_project/src/main/java/sde_project/sde_project/App.java
+++ sde_project/src/main/java/sde_project/sde_project/App.java
@@ -1,13 +1,29 @@
 package sde_project.sde_project;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.NotFoundException;
+
+public class App {
+	public static void main(String[] args) {
+		ClassPool pool = ClassPool.getDefault();
+		try {
+			CtClass cc = pool.get("com.kenai.jffi.Array");
+			// TODO Do some meaningful - just for testing purposes.
+			cc.writeFile("generated_binary_files");
+		} catch (CannotCompileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
